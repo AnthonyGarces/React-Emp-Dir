eb3590cfca66641aeb4c1a9b3b3dc30a6b450ec0
Esteban Garces <AnthonyGarces@users.noreply.github.com>
2020-03-07 10:03:13 -0600

    Completed header plus minor styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,17 @@
 import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import './header.css';
 
 
 function Header() {
     return (
-        <div>
-            Hello World
-        </div>
-        // <Jumbotron fluid>
-        //     <Container fluid>
-        //         <h1>Employee Directory</h1>
-        //         <p>Click on carrots to filter by heading or use the search box</p>
-        //     </Container>
-        // </Jumbotron>
+        <Jumbotron fluid className="header text-center">
+            <Container fluid>
+                <h1>Employee Directory</h1>
+                <p>Click on carrots to filter by heading or use the search box</p>
+            </Container>
+        </Jumbotron>
     )
 }
 
