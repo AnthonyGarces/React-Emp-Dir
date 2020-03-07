fb7424f4f5f97ab20d787d4980d3e84517072cdf
Esteban Garces <AnthonyGarces@users.noreply.github.com>
2020-03-06 21:01:49 -0600

    formatted how data is presented in the table rows

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,13 +1,49 @@
 import React from 'react';
+import Table from 'react-bootstrap/Table'; 
 
 function ResultList(props) {
 
     if(!props.data) {
         return <div>Loading...</div>
     }
-    console.log(props.data[0])
+
+    // function Employee(props) {
+    //     return (
+    //         <tr>
+    //             <td>{props.data.picture.thumbnail}</td>
+    //             <td>{props.data.name.first + "" + props.data.name.last}</td>
+    //             <td>{props.data.dob}</td>
+    //             <td>{props.data.phone}</td>
+    //             <td>{props.data.email}</td>
+    //         </tr>
+    //     )
+    // };
+
+    console.log(props.data[0]);
+    console.log(props.data[0].picture.thumbnail);
+    console.log(props.data[0].name.first)
     return (
-        <div>{JSON.stringify(props)}</div>
+        <Table>
+            <thead>
+                <tr>
+                    <th>Photo</th>
+                    <th>Name</th>
+                    <th>Date of Birth</th>
+                    <th>Phone Number</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                {<tr>
+                    <td><img src={props.data[0].picture.thumbnail} alt="pic"></img></td>
+                    <td>{props.data[0].name.first + " " + props.data[0].name.last}</td>
+                    <td>{props.data[0].dob.date.slice(0, 10)}</td>
+                    <td>{props.data[0].phone}</td>
+                    <td>{props.data[0].email}</td>
+                </tr>}
+                {/* {props.data && props.data.map(i => <Employee data={i}/>)} */}
+            </tbody>
+        </Table>
     )
 }
 
